b1ab8274fdec8ae6644216f4ab0b6a8d102917e9
KoKyungBeom <165968631+KoKyungBeom@users.noreply.github.com>
2024-08-07 19:57:14 +0900

    [level 1] Title: 기사단원의 무기, Time: 203.03 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int[] array = new int[number];
+        
+        for (int i = 0; i < number; i++) {
+            if (getDivisorCount(i + 1) > limit) {
+                array[i] = power;
+            } else {
+                array[i] = getDivisorCount(i + 1);
+            }
+        }
+        
+        int answer = 0;
+        
+        for (int i : array) {
+            answer += i;
+        }
+        
+        return answer;
+    }
+    public static int getDivisorCount (int num) {
+        int result = 0;
+    
+        for (int i = 1; i * i <= num; i++) {
+            if (i * i == num) {
+                result ++;
+            } 
+            else if(num  % i == 0) {
+                result += 2;
+            }
+        }
+        return result;
+    }
+}

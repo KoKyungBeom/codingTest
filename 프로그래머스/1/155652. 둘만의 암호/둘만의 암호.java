fda4b2fd2c6052aac34b126239a2f1d227f0523e
KoKyungBeom <165968631+KoKyungBeom@users.noreply.github.com>
2024-08-18 02:02:25 +0900

    [level 1] Title: 둘만의 암호, Time: 13.82 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        return password(s,skip,index);
+    }
+    public static String password(String s, String skip, int index) {
+    String answer = "";
+    char[] sChar = s.toCharArray();
+    char[] skipChar = skip.toCharArray();
+
+    List<Character> answerChar = new ArrayList<>();
+    
+    boolean[] skipCheck = new boolean[26];
+    for (char ch : skipChar) {
+        skipCheck[ch - 'a'] = true;
+    }
+
+    for (char c : sChar) {
+        int count = 0;
+        char newChar = c;
+        
+        while (count < index) {
+            newChar++;
+            if (newChar > 'z') {
+                newChar = 'a';
+            }
+            if (!skipCheck[newChar - 'a']) {
+                count++;
+            }
+        }
+        answerChar.add(newChar);
+    }
+
+    for (char ch : answerChar) {
+        answer += ch;
+    }
+
+        return answer;
+    }
+}

bdfa296f628bcadc16ae99d618aa43271388bf5d
KoKyungBeom <165968631+KoKyungBeom@users.noreply.github.com>
2024-11-02 23:41:37 +0900

    [level 1] Title: 소수 만들기, Time: 6.52 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int[] nums) {
-        //nums 배열에서 3개의 숫자를 뽑아서 만들수있는 수들의 배열을 만든다(nCr 크기의 배열 n = nums.lenght r = 3)
+        //nums 배열에서 3개의 숫자를 뽑아서 만들수있는 수들을 소수인지 판별하고 맞으면 카운트를 증가시킨다
         //소수인지 판별하는 메서드를 만든다
-        //새로만든 배열을 순회하며 소수판별 메서드를 통해서 소수인지 검증한뒤 맞다면 카운트를 증가시킨다
         int answer = 0;
+        
         for (int i = 0; i < nums.length - 2; i++){
             for (int j = i+1; j < nums.length; j++){
                 for (int k = j+1; k < nums.length; k++){

69d61ec40a39d76757769013bab4c39bb75e1653
KoKyungBeom <165968631+KoKyungBeom@users.noreply.github.com>
2024-11-06 00:35:51 +0900

    [level 1] Title: 명예의 전당 (1), Time: 7.53 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        
+        int[] answer = new int[score.length];
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < score.length; i++) {
+            stack.push(score[i]);
+            
+            Collections.sort(stack, Collections.reverseOrder());
+            
+            if (stack.size() > k) {
+                stack.pop();
+            }
+            
+            answer[i] = stack.peek();
+        }
+        
+        return answer;
+    }
+}

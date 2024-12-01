fead033f790dc887250d689783b61e52ea55160d
KoKyungBeom <165968631+KoKyungBeom@users.noreply.github.com>
2024-12-01 17:45:33 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -3,7 +3,6 @@ class Solution {
         int evenCount = 0;
         int oddCount = 0;
         
-        // num_list 배열을 순회하면서 짝수와 홀수 개수를 셈
         for (int num : num_list) {
             if (num % 2 == 0) {
                 evenCount++;
@@ -12,7 +11,6 @@ class Solution {
             }
         }
         
-        // 짝수 개수와 홀수 개수를 담은 배열 반환
         return new int[]{evenCount, oddCount};
     }
 }
